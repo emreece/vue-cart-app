c312ab968ced65ab500d084a15d9acde998c1ece
unknown <emre.ece@erstesoftware.com>
2020-11-04 00:14:49 +0300

    emit and handle function is added

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+Vue.component('VueCart', {
+    props: {
+        cart: {type:Array, required:true, default:() => {return []} },
+        // targetcart: {type:Array, required:true, default:() => {return []}},
+        title: {type:String, required:true},
+        buttontext: {type:String, required:true},
+        carttype: {type:String, required:true},
+        showtotal: {type:Boolean, required:false, default:true}
+    },
+    computed: {
+        cartTotal() {
+            let total = 0;
+            this.cart.forEach(element => {
+                total += parseFloat(element.price);
+            });
+            return total.toFixed(2);
+        },
+        cartsCount() {
+            let total = 0;
+            let data = {};
+            this.cart.forEach(element => {
+                total++;
+            });
+            if(total == 0) { data.message = 'No Item'; data.number = '';} 
+            else if (total == 1) { data.message = ' item'; data.number = total;  } 
+            else { data.message = 'items'; data.number = total;  }
+            return data;
+        },
+        isProducts() {
+            return this.cartType === 'products'
+        },
+        isCart() {
+            return this.cartType === 'cart'
+        }
+    },
+    methods: {
+        moveToProducts(index) {
+            const item = this.cart.splice(index,1)[0];
+            // this.targetcart.push(item); moved to handle function
+            this.$emit('movingproduct', item, this.carttype)
+        }
+    },
+    template: `<div class="cart-wrapper">
+                <h2>{{title}} ({{cartsCount.number}} {{cartsCount.message}}) <span v-if="showtotal">Total:{{cartTotal}} $</span></h2> 
+                   <div class="cart">
+                       <div class="item card my-2 p-3" v-for="(item,index)  in cart">
+                          <h1>{{item.name}}</h1>
+                          <p>{{item.price}} \$</p>
+                          <div v-if="item.isAvaliable">In Stock</div>
+                          <div v-if="!item.isAvaliable">No Stock</div>
+                          <a v-if="item.isAvaliable" href="#" v-on:click="moveToProducts(index)">{{buttontext}}</a>
+                       </div>
+                   </div>
+                </div>`
+                });
 window.addEventListener('load', () => {
    window.vue =  new Vue({
         el: '#app',
@@ -8,45 +63,14 @@ window.addEventListener('load', () => {
             cart:[]
         },
         methods: {
-            moveToCart(index) {
-                const item = this.products.splice(index,1)[0];
-                this.cart.push(item);
-            },
-            moveToProducts(index) {
-                const item = this.cart.splice(index,1)[0];
-                this.products.push(item);
+            handlemovingproduct(item, cartType) {
+                if (cartType === 'products') {
+                    this.cart.push(item);
+                }
+                else {
+                    this.products.push(item);
+                }
             }
-        },
-        computed: {
-            cartTotal() {
-                let total = 0;
-                this.cart.forEach(element => {
-                    total += parseFloat(element.price);
-                });
-                return total.toFixed(2);
-            },
-            cartsCount() {
-                let total = 0;
-                let data = {};
-                this.cart.forEach(element => {
-                    total++;
-                });
-                if(total == 0) { data.message = 'No Item'; data.number = '';} 
-                else if (total == 1) { data.message = ' item'; data.number = total;  } 
-                else { data.message = 'items'; data.number = total;  }
-                return data;
-            },
-            productsCount() {
-                let total = 0;
-                let data = {};
-                this.products.forEach(element => {
-                    total++;
-                });
-                if(total == 0) { data.message = 'No Item'; data.number = '';} 
-                else if (total == 1) { data.message = ' item'; data.number = total;  } 
-                else { data.message = 'items'; data.number = total;  }
-                return data;
-            },
         },
         created() {
             fetch('./data.json')
